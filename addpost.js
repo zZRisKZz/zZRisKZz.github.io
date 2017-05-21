2915e51eaf7b031dc55c41ec145ed41e25028c16
zZRisKZz <zzrisk97zz@gmail.com>
2017-05-21 22:09:52 +0100

    Add files via upload

---
+++ addpost.js
@@ -0,0 +1,54 @@
+// NAME: entry.1409408803
+// TIPO: entry.1646540622
+// DATO: entry.1636019526
+// AVATAR: entry.638172567
+// FILE: https://docs.google.com/a/ull.edu.es/forms/d/e/1FAIpQLSeMKJCJQolk1JapvJYhEQMl_mhtW1kbeR2sNlUMVvxUL7sQew/formResponse
+
+function post(){
+    var inputName = encodeURIComponent($('#nombre').val());
+    var inputType = encodeURIComponent($('#tipo').val());
+    var inputData = encodeURIComponent($('#datos').val());
+
+    var entryName = "&entry.1409408803=";
+    var entryTipo = "&entry.1646540622=";
+    var entryData = "&entry.1636019526=";
+
+    var baseURL = 'https://docs.google.com/a/ull.edu.es/forms/d/e/1FAIpQLSeMKJCJQolk1JapvJYhEQMl_mhtW1kbeR2sNlUMVvxUL7sQew/formResponse?';
+    var submitRef = '&submit=Submit';
+    var submitURL = (baseURL + entryName + inputName + entryTipo + inputType + entryData + inputData + submitRef);
+    console.log(submitURL);
+    newW = window.open(submitURL);
+    newW.close();
+}
+
+function postText(){
+  var cookie = readCookie('login');
+  var inputName = "";
+  var i = 0;
+  while(cookie[i] != ","){
+    inputName = (inputName + cookie[i]);
+    i++;
+  }
+  while(cookie[i] != ","){ i++;}
+  var inputAvatar = "";
+  while(cookie[i] != " "){
+    inputAvatar = (inputAvatar + cookie[i]);
+    i++;
+  }
+
+
+  var inputType = "txt";
+  var inputData = encodeURIComponent($('#txt').val());
+
+  var entryName = "&entry.1409408803=";
+  var entryTipo = "&entry.1646540622=";
+  var entryData = "&entry.1636019526=";
+  var entryAvatar = "&entry.638172567";
+
+  var baseURL = 'https://docs.google.com/a/ull.edu.es/forms/d/e/1FAIpQLSeMKJCJQolk1JapvJYhEQMl_mhtW1kbeR2sNlUMVvxUL7sQew/formResponse?';
+  var submitRef = '&submit=Submit';
+  var entry =(entryName+inputName + entryTipo+inputType + entryData+inputData + entryAvatar+inputAvatar);
+  var submitURL = (baseURL + entry + submitRef);
+  console.log(submitURL);
+  newW = window.open(submitURL);
+}
